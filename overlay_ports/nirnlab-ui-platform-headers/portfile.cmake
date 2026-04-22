a4560fd192724dbf2b4dd6ff539f8c45d7393c56
DexOfTheWild <alexxr87@gmail.com>
2026-04-22 17:19:09 -0400

    feat: add optional external targeted nirnlab runtime build
    (cherry picked from commit b08da5a3673f0953f163c1b7fbfa4dfa5998394a)

--- overlay_ports/nirnlab-ui-platform-headers/portfile.cmake
+++ overlay_ports/nirnlab-ui-platform-headers/portfile.cmake
@@ -1,11 +1,19 @@
-vcpkg_from_github(
+# Pin the fork that contains microphone permission support until upstream catches up.
+vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO kkEngine/NirnLabUIPlatform
-  REF 21c1adaa719245e039566f2426bc7cd546005677  # ver3.1PR + a few commits, will likely be tagged as ver3.1
-  SHA512 7acc44e6af661b10a1082fe5a82ddbe0276912d04622c2295c2c3cd61be27e24b14764d4c0c2b767b9ad8bc567a0d57ccba7712067601a9b1595383cd7e2420e
-  HEAD_REF main
+  URL https://github.com/DexOfTheWild/NirnLabUIPlatform.git
+  REF 1f5a70b7a3334d33a67a6f3bc2a9f4e24ae2db48
+  FETCH_REF microphone-access
+  HEAD_REF microphone-access
 )
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(COPY "${SOURCE_PATH}/src/UIPlatform/NirnLabUIPlatformAPI" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+# When developing next to sibling checkout, overlay headers so Skyrim Platform can compile against unpushed API changes (e.g. Settings.h).
+set(_nl_local_api "${CMAKE_CURRENT_LIST_DIR}/../../NirnLabUIPlatform/src/UIPlatform/NirnLabUIPlatformAPI")
+if(EXISTS "${_nl_local_api}/Settings.h")
+  message(STATUS "nirnlab-ui-platform-headers: overlay local NirnLabUIPlatformAPI from ${_nl_local_api}")
+  file(COPY "${_nl_local_api}/." DESTINATION "${CURRENT_PACKAGES_DIR}/include/NirnLabUIPlatformAPI")
+endif()
